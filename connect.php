5ae8e871a90ed6e64ec6f566ea5b23e6f70530e8
Ulises Gomez - Jaramillo <ulisesgomezjaramillo@gmail.com>
2015-04-22 19:26:16 -0700

    video 4

---
+++ connect.php
@@ -0,0 +1,13 @@
+<?php
+$mysqli = new mysqli('localhost', 'root', 'root', 'todo2');
+
+if ($mysqli->connect_error) {
+	die('The connection dun did dieded (' . $mysqli->connect_errno .')'
+		. $mysqli->connect_error);
+}
+else{
+	echo "The connection was made m8";
+}
+$mysqli->close();
+
+?>
