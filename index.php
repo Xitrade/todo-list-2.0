79ffb28863c4a52efa88a6bf668db4562e883ec2
Ulises Gomez - Jaramillo <ulisesgomezjaramillo@gmail.com>
2015-04-23 11:51:37 -0700

    finished video 6

--- index.php
+++ index.php
@@ -6,7 +6,14 @@
 </head>
 <body>
 <div class="wrap">
-	
+	<div class="task-list">
+		<ul>
+			<?php require("includes/connect.php"); ?>
+		</ul>	
+	</div>
+	<form class="add-new-task" autocomplete="off">
+	<input type="text" name="new-task" placeholder="Type something m89999999999999999999999999999999999999999999999999999999999999999999999999999999"/>
+	</form>
 </div>
 </body>
 </html>
